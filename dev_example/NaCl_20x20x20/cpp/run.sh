ccbdeaafc1a4d7e0876ea83319f8eaf5d397919d
ArisSgouros <arissgouros@gmail.com>
2023-10-31 10:58:03 +0200

    test: refresh tests

--- dev_example/NaCl_20x20x20/cpp/run.sh
+++ dev_example/NaCl_20x20x20/cpp/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+path_gij="../../../cpp/gij_partial"
 path_python="../../../pyxrd/"
 path_data="../gen/"
 
 # compute rdf
-python $path_python/pyrdf.py $path_data/o.nacl.dat $path_data/o.nacl.lammpstrj 25.0 0.1 1 1 -atomtype='full' -rdffile='o.rdf' > o.log_rdf
+$path_gij 25.0 0.1 1 FULL $path_data/o.nacl.dat $path_data/o.nacl.lammpstrj gij_pairs.dat o.rdf > o.log_rdf
 
 # compute xrd
 python $path_python/pyxrd.py $path_data/o.nacl.dat o.rdf.dat ../form_factors.dat 25.0 0.1 20.0 0.01 -atomtype='full' > o.log_xrd
--- dev_example/Ni_10x10x10/cpp/run.sh
+++ dev_example/Ni_10x10x10/cpp/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+path_gij="../../../cpp/gij_partial"
 path_python="../../../pyxrd/"
 path_data="../gen/"
 
 # compute rdf
-python $path_python/pyrdf.py $path_data/o.ni.dat $path_data/o.ni.lammpstrj 17.6 0.1 1 1 -atomtype='full' -rdffile='o.rdf' > o.log_rdf
+$path_gij 17.6 0.1 1 FULL $path_data/o.ni.dat $path_data/o.ni.lammpstrj gij_pairs.dat o.rdf > o.log_rdf
 
 # compute xrd
 python $path_python/pyxrd.py $path_data/o.ni.dat o.rdf.dat ../form_factors.dat 17.6 0.1 20.0 0.05 -atomtype='full' > o.log_xrd
