643a8ca6723e2b97d3bad8e77f0ba28833b93c94
ArisSgouros <arissgouros@gmail.com>
2023-11-07 14:56:49 +0200

    test(tool/group_ff): update tests

--- tool/group_form_factor/example/c-h/run.sh
+++ tool/group_form_factor/example/c-h/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-python ../../group_ff.py group_c-h.xyz form_factors.dat 20.0 0.1 --orig="0.0,0.0,0.0" --scheme="narten" --ff_group_file="o.ff_group_narten.dat" > o.log_narten
 
-python ../../group_ff.py group_c-h.xyz form_factors.dat 20.0 0.1 --orig="0.0,0.0,0.0" --scheme="sum" --ff_group_file="o.ff_group_sum.dat" > o.log_sum
+# group form factor w/ narten scheme
+python ../../group_ff.py group_c-h.xyz form_factors.dat 20.0 0.1 --ff_group_file="o.ff_group_narten.dat" \
+    --scheme="narten" --init_guess="1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0" --orig="0.0,0.0,0.0" > o.log_narten
+
+# group form factor w/ sum scheme
+python ../../group_ff.py group_c-h.xyz form_factors.dat 20.0 0.1 --ff_group_file="o.ff_group_sum.dat" \
+    --scheme="sum" --init_guess="1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0" > o.log_sum
